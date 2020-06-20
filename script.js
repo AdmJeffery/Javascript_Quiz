c207547fd9d32ce49b31544d7675f13151a24804
Ammon Jeffery <savingphace@gmail.com>
2020-06-19 19:32:16 -0600

    began gameOver function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//window.onload=function(){
-    //debugger
+
 var startButton = document.querySelector("#start-button");
 var timer = document.querySelector("#timer");
 var messageBox = document.querySelector("#message-box");
@@ -19,44 +18,43 @@ var textInput
 
 var currentQuestion = 0;
 var score = 0;
-var totQuestions = questions.length;
+
 
 
 
 var questions = [{
-  "question":"Inside which HTML element do we put a Javascript tag?",
-  "option1": "<head>",
-  "option2":"<script>",
-  "option3":"<body>",
-  "option4" : "<div>",
-  "answer" : "2"
+  question :"Inside which HTML element do we put a Javascript tag?",
+  option1: "<head>",
+  option2:"<script>",
+  option3:"<body>",
+  option4: "<div>",
+  answer: "2"
 },{
-  "question":"Where is the correct place to put a Javascript tag?",
-  "option1": "<head>",
-  "option2":"<script>",
-  "option3":"<body>",
-  "option4" : "<div>",
-  "answer" : "2",
+  question:"Where is the correct place to put a Javascript tag?",
+  option1: "<head>",
+  option2:"<script>",
+  option3:"<body>",
+  option4:"<div>",
+  answer: "2"
 },{
-  "question":"Does an external Javascript file need a <script> tag?",
-  "option1": "I don't know",
-  "option2":"Who cares?",
-  "option3":"Yes",
-  "option4" : "No",
-  "answer" : "2",
+  question :"Does an external Javascript file need a <script> tag?",
+  option1: "I don't know",
+  option2:"Who cares?",
+  option3:"Yes",
+  option4:"No",
+  answer: "4"
 },{
-  "question":"What is the syntax for creating a function in JS?",
-  "option1": "funtion myFunction()",
-  "option2":"createFunction myFunction()",
-  "option3":"function:myFunction()",
-  "option4" : "function = myFunction",
-  "answer" : "1",
+  question :"What is the syntax for creating a function in JS?",
+  option1 : "funtion myFunction()",
+  option2 :"createFunction myFunction()",
+  option3 :"function:myFunction()",
+  option4  : "function = myFunction",
+  answer : "1"
 }];
+var totQuestions = questions.length;
 
-//if (startButton) {
     startButton.addEventListener("click",startUp);
-//};
-//};
+
 function startUp() {
 var timeoutHandle;
 
@@ -89,15 +87,17 @@ var timeoutHandle;
 
   countdown(2);
   loadQuestion(currentQuestion);
+  
 }
 
 function loadQuestion (questionIndex) {
+
   let q = questions[questionIndex];
-  question.El.textContent = (questionIndex + 1) + ". " + q.question;
+  questionEl.innerText = (questionIndex + 1) + ". " + q.question;
   opt1.textContent = q.option1;
   opt2.textContent = q.option2;
   opt3.textContent = q.option3;
-  opt4.textcontent = q.option4;
+  opt4.textContent = q.option4;
   
 };
 
@@ -121,7 +121,13 @@ function loadNextQuestion () {
   }
 
   if (currentQuestion == totQuestions){
-    quizContainer.style.display = "none";
-
+    gameOver();
+    
+    return;
   }
+  loadQuestion(currentQuestion);
+}
+
+function gameOver () {
+
 }
